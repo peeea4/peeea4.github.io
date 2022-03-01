52d92aca2406887c2c623eb68b5b274cc0f3d82a
peeea4 <peeea4@gmail.com>
2022-03-01 11:24:36 +0300

    Added JS Logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
-function defaultTask(cb) {
-    const HLTV = require('hltv-api').default
-    const express = require('express')
-    const app = express()
+const {src, dest, watch} = require('gulp');
+const minifyJs = require('gulp-uglify');
+const sourceMaps = require('gulp-sourcemaps')
+const concat = require('gulp-concat');
 
-    app.get('/results', async (req, res) => {
-        const results = await HLTV.getResults()
-        res.json(results)
-    })
-    console.log(132123);
-    cb();
+const HLTV = require('hltv-api').default
+const express = require('express')
+const app = express()
+
+const jsFiles = ['./src/script.js', './node_modules/hltv-api/dist/**/*.js'];
+const bundleJs = () => {
+    return src(jsFiles)
+        .pipe(sourceMaps.init())
+        .pipe(minifyJs())
+        .pipe(concat('bundle.js'))
+        .pipe(sourceMaps.write())
+        .pipe(dest('./dist/src/'));
 }
 
-exports.default = defaultTask
+const devWatch = () => {
+    watch('./node_modules/hltv-api/dist/**/*.js', bundleJs);
+}
+
+const showNews = (app) => {
+    app.get('/', async (req, res) => {
+        const news = await HLTV.getNews()
+        res.json(news)
+        console.log(res, news);
+    })
+}
+exports.default = showNews;
+exports.bundleJs = bundleJs;
+exports.devWatch = devWatch;
--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
-let btns = Array.from(document.querySelectorAll(".show-text"));
-btns.forEach(element => {
+import { usersList } from "./users.js"
+
+let btnsShow = Array.from(document.querySelectorAll(".show-text"));
+btnsShow.forEach(element => {
     element.addEventListener("click", () => {
         element.classList.toggle("active");
         let panel = element.nextElementSibling;
@@ -11,8 +13,8 @@ btns.forEach(element => {
     });
 });
 
-let btnsTry = Array.from(document.querySelectorAll(".btn"));
-btnsTry.forEach(element => {
+let btnsNavigate = Array.from(document.querySelectorAll(".btn"));
+btnsNavigate.forEach(element => {
     if(element.classList.contains("try")) {
         element.addEventListener("click", () => {
             document.location.href = "/index.html#compare"
@@ -23,3 +25,53 @@ btnsTry.forEach(element => {
         })
     }
 })
+
+
+let btnCompare = document.querySelector(".enter");
+btnCompare.addEventListener("click", () => {
+    let inputStar = document.querySelector(".star").value;
+    searchStar(inputStar);
+})
+
+function searchStar(inputStar) {
+    for (let index = 1; index < 6; index++) {
+        if(usersList[index].nickname == inputStar) {
+            createStarCard(usersList[index]);
+        }
+    }
+}
+let x = document.querySelector(".user");
+function createStarCard(array) {
+    for (let key in array) {
+        switch(key){
+            case "image":
+                let statName = document.createElement("img");
+                statName.classList.add('user-image');
+                console.log("hi");
+                statName.src = array[key];
+                x.appendChild(statName);
+                break;
+            case "id":
+                console.log(`Look at this ID: ${array[key]}`);
+                break;
+            case "id":
+                console.log(`Look at this ID: ${array[key]}`);
+                break;
+            case "id":
+                console.log(`Look at this ID: ${array[key]}`);
+                break;
+            case "id":
+                console.log(`Look at this ID: ${array[key]}`);
+                break;
+
+        }
+        // console.log(key, array[key]);
+    }
+}
+// создаём элементы с именами - key и блоки со значениями - ${array[key]}
+
+
+
+// function createCard() {
+//     let cardContainer = document.querySelector(".compare-saved");
+// }

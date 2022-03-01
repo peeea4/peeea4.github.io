eda6b9cf4f19534ce72b526c589eeccd10f3c9e1
peeea4 <peeea4@gmail.com>
2022-03-01 20:06:13 +0300

    01.03.22 added webkit

--- src/script.js
+++ src/script.js
@@ -2,71 +2,115 @@ import { usersList } from "./users.js"
 
 let btnsShow = Array.from(document.querySelectorAll(".show-text"));
 btnsShow.forEach(element => {
-    element.addEventListener("click", () => {
-        element.classList.toggle("active");
-        let panel = element.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+	element.addEventListener("click", () => {
+		element.classList.toggle("active");
+		let panel = element.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
 });
 
 let btnsNavigate = Array.from(document.querySelectorAll(".btn"));
 btnsNavigate.forEach(element => {
-    if(element.classList.contains("try")) {
-        element.addEventListener("click", () => {
-            document.location.href = "/index.html#compare"
-        })
-    } else if (element.classList.contains("example")) {
-        element.addEventListener("click", () => {
-            document.location.href = "/index.html#tutorial"
-        })
-    }
+	if (element.classList.contains("try")) {
+		element.addEventListener("click", () => {
+			document.location.href = "/index.html#compare"
+		})
+	} else if (element.classList.contains("example")) {
+		element.addEventListener("click", () => {
+			document.location.href = "/index.html#tutorial"
+		})
+	}
 })
 
 
 let btnCompare = document.querySelector(".enter");
 btnCompare.addEventListener("click", () => {
-    let inputStar = document.querySelector(".star").value;
-    searchStar(inputStar);
+	let inputStar = document.querySelector(".star").value;
+	searchStar(inputStar);
 })
 
 function searchStar(inputStar) {
-    for (let index = 1; index < 6; index++) {
-        if(usersList[index].nickname == inputStar) {
-            createStarCard(usersList[index]);
-        }
-    }
+	for (let index = 1; index < 6; index++) {
+		if (usersList[index].nickname == inputStar) {
+			createStarCard(usersList[index]);
+		}
+	}
 }
-let x = document.querySelector(".user");
+let serverContainer = document.querySelector(".server");
 function createStarCard(array) {
-    for (let key in array) {
-        switch(key){
-            case "image":
-                let statName = document.createElement("img");
-                statName.classList.add('user-image');
-                console.log("hi");
-                statName.src = array[key];
-                x.appendChild(statName);
-                break;
-            case "id":
-                console.log(`Look at this ID: ${array[key]}`);
-                break;
-            case "id":
-                console.log(`Look at this ID: ${array[key]}`);
-                break;
-            case "id":
-                console.log(`Look at this ID: ${array[key]}`);
-                break;
-            case "id":
-                console.log(`Look at this ID: ${array[key]}`);
-                break;
+	let statContainer = document.createElement("div");
+	statContainer.classList.add('stat')
+	serverContainer.appendChild(statContainer);
+	for (let key in array) {
+		switch (key) {
+			case "image":
+				let statName = document.createElement("img");
+				statName.classList.add('user-image');
+				statName.src = array[key];
+				serverContainer.appendChild(statName);
+				break;
+			case "age":
+				let statAge = document.createElement("p");
+				statAge.classList.add('description', 'large');
+				statAge.innerHTML = array[key];
+				statContainer.appendChild(statAge);
+				break;
+			case "rating":
+				let statRating = document.createElement("p");
+				statRating.classList.add('description', 'large');
+				statRating.innerHTML = array[key];
+				statContainer.appendChild(statRating);
+				break;
+			case "impact":
+				let statImpact = document.createElement("p");
+				statImpact.classList.add('description', 'large');
+				statImpact.innerHTML = array[key];
+				statContainer.appendChild(statImpact);
+				break;
+			case "dpr":
+				let statDpr = document.createElement("p");
+				statDpr.classList.add('description', 'large');
+				statDpr.innerHTML = array[key];
+				statContainer.appendChild(statDpr);
+				break;
+			case "apr":
+				let statApr = document.createElement("p");
+				statApr.classList.add('description', 'large');
+				statApr.innerHTML = array[key];
+				statContainer.appendChild(statApr);
+				break;
+			case "kast":
+				let statKast = document.createElement("p");
+				statKast.classList.add('description', 'large');
+				statKast.innerHTML = array[key];
+				statContainer.appendChild(statKast);
+				break;
+			case "kpr":
+				let statKpr = document.createElement("p");
+				statKpr.classList.add('description', 'large');
+				statKpr.innerHTML = array[key];
+				statContainer.appendChild(statKpr);
+				break;
+			case "headshots":
+				let statHeadshots = document.createElement("p");
+				statHeadshots.classList.add('description', 'large');
+				statHeadshots.innerHTML = array[key];
+				statContainer.appendChild(statHeadshots);
+				break;
+			case "mapsPlayed":
+				let statMapsPlayed = document.createElement("p");
+				statMapsPlayed.classList.add('description', 'large');
+				statMapsPlayed.innerHTML = array[key];
+				statContainer.appendChild(statMapsPlayed);
+				break;
 
-        }
-        // console.log(key, array[key]);
-    }
+		}
+		// console.log(key, array[key]);
+	}
 }
 // создаём элементы с именами - key и блоки со значениями - ${array[key]}
 
